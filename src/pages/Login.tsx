ad772e5b703be57406d2813d48dbe6c3d8cd446a
Pinton04 <robertpasukin042518@gmail.com>
2025-02-19 21:18:24 +0800

    Added changes in LLogin.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -13,6 +13,7 @@ import {
 const Login: React.FC = () => {
   const navigation = useIonRouter();
   const doLogin = () => {
+    ( " " )
       navigation.push('/it35-lab/app','forward','replace');
   }
   return (
