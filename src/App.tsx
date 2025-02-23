4c8ddcbb9fc683af2e7c9c959653c1a2ee3fc64b
Pinton04 <robertpasukin042518@gmail.com>
2025-02-23 21:16:49 +0800

    Added updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -42,8 +42,10 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonRouterOutlet>
       <Route exact path="/it35-lab" component={Login} />
-      <Route exact path="/it35-lab" component={Menu} />
-      </IonRouterOutlet>
+      <Route path="/it35-lab/app" component={Menu} />
+        </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
 );
+
+export default App;
