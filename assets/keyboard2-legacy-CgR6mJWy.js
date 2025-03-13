ec12a4312d449f983e577904786ab935a12bedce
Pinton04 <robertpasukin042518@gmail.com>
2025-03-13 22:37:30 +0800

    Updates

--- assets/keyboard2-legacy-CgR6mJWy.js
+++ assets/keyboard2-legacy-CgR6mJWy.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy--1g2_TXX.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-Dr0cMTKH.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-DUhSuGPR.js
+++ assets/swipe-back-legacy-DUhSuGPR.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy--1g2_TXX.js","./index3-legacy-CJhLiK6A.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-Dr0cMTKH.js","./index3-legacy-AsBhJobn.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
