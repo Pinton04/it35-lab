d713d857599903a0e61069ff88ba9bd6a30a2aa2
Pinton04 <robertpasukin042518@gmail.com>
2025-04-03 08:59:27 +0800

    Updates

--- assets/ios.transition-legacy-C5SBq7bB.js
+++ assets/ios.transition-legacy-C5SBq7bB.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ClKEwsTa.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-BRbaN-QL.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-uZQgsArH.js
+++ assets/swipe-back-legacy-uZQgsArH.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ClKEwsTa.js","./index3-legacy-HB9JYlMh.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-BRbaN-QL.js","./index3-legacy-BPrzkoJP.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
