efc5af3a70eec041c54fa1eba72649a81be56f36
Pinton04 <robertpasukin042518@gmail.com>
2025-04-03 08:29:46 +0800

    Updates

--- assets/swipe-back-legacy-Dcd1cZBH.js
+++ assets/swipe-back-legacy-Dcd1cZBH.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C3H13d_y.js","./index3-legacy-a__rjDuv.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-ClKEwsTa.js","./index3-legacy-HB9JYlMh.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
