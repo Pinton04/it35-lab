6c06746f03e8855f6e2db008204f747924bf03c2
Pinton04 <robertpasukin042518@gmail.com>
2025-04-30 21:36:52 +0800

    Updates

--- assets/ios.transition-legacy-Dxqg84ZT.js
+++ assets/ios.transition-legacy-Dxqg84ZT.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-dA5O-fVW.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CCnzkL7M.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-DwW6Onz0.js
+++ assets/swipe-back-legacy-DwW6Onz0.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-dA5O-fVW.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-CCnzkL7M.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
